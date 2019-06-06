39c1dd642919b6e97a497969995ec69c4898a322
Philip Nunoo <phanunoo@gmail.com>
2019-06-06 22:41:20 +0000

    retrieve and return only logged in user data

--- MeteorApp/imports/api/notes/server/publications.js
+++ MeteorApp/imports/api/notes/server/publications.js
@@ -2,9 +2,16 @@ import { Meteor } from 'meteor/meteor';
 import Notes from '../index';
 
 Meteor.publishComposite('notes.all', function () {
-    return ({
-        find() {
-            return Notes.find();
+  return ({
+    find() {
+      return Notes.find({ createdBy: this.userId });
+    },
+    children: [
+      {
+        find(note) {
+          return Meteor.users.find({ _id: note.createdBy });
         }
-    })
+      }
+    ]
+  });
 });
