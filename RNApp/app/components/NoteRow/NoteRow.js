897097ef442d1f80eb4eeef68839a54121753df8
Philip Nunoo <phanunoo@gmail.com>
2019-06-07 09:36:56 +0000

    modify footer to reflect conversation logs

--- RNApp/app/components/NoteRow/NoteRow.js
+++ RNApp/app/components/NoteRow/NoteRow.js
@@ -2,12 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import moment from 'moment';
 import { View, Text, StyleSheet } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import MaterialCommunityIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 // import Meteor from 'react-native-meteor';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 10,
+    paddingBottom: 5,
     backgroundColor: '#FFFFFF',
     elevation: 2,
     borderRadius: 5,
@@ -28,7 +31,7 @@ const styles = StyleSheet.create({
     borderTopColor: '#000',
     borderTopWidth: 1,
     marginTop: 10,
-    padding: 5,
+    padding: 8,
     paddingLeft: 10,
     paddingRight: 10,
     alignItems: 'stretch',
@@ -37,6 +40,12 @@ const styles = StyleSheet.create({
   },
   footerText: {
     fontSize: 11,
+  },
+  footerLeft: {
+    flexDirection: 'row',
+  },
+  messageText: {
+    marginRight: 5,
   },
   createdAt: {
     fontSize: 11,
@@ -48,7 +57,7 @@ const styles = StyleSheet.create({
 //   return user ? user.emails[0].address : '-';
 // };
 
-const NoteRow = ({ title, details, createdBy, createdAt }) => (
+const NoteRow = ({ title, details, mail, createdAt }) => (
   <View style={styles.container}>
     <View>
       <Text style={styles.title}>{title}</Text>
@@ -56,12 +65,27 @@ const NoteRow = ({ title, details, createdBy, createdAt }) => (
     </View>
     <View style={styles.footer}>
       <View>
+        <Icon
+          name="snapchat-ghost"
+          style={{ color: '#220A82' }}
+        />
       </View>
-      <Text style={[styles.footerText, styles.createdAt]}>
-        {createdAt ?
-        moment(createdAt).format('YYYY-MM-DD')
-        : '-'}
-      </Text>
+      <View style={styles.footerLeft}>
+        {mail &&
+        <Text style={[styles.footerText, styles.messageText]}>
+          {mail.messages.length}
+          {' '}
+          <MaterialCommunityIcon
+            name="chat"
+            style={{ marginRight: 10, color: '#000' }}
+          />
+        </Text>}
+        <Text style={[styles.footerText, styles.createdAt]}>
+          {createdAt ?
+          moment(createdAt).format('LL')
+          : '-'}
+        </Text>
+      </View>
     </View>
   </View>
 );
@@ -71,6 +95,13 @@ NoteRow.propTypes = {
   title: PropTypes.string,
   createdAt: PropTypes.string,
   createdBy: PropTypes.string,
+  mail: PropTypes.shape({
+    messageId: PropTypes.string.isRequired,
+    subject: PropTypes.string.isRequired,
+    messages: PropTypes.arrayOf({
+      userId: PropTypes.string,
+    }),
+  }),
 };
 
 NoteRow.defaultProps = {
@@ -78,6 +109,7 @@ NoteRow.defaultProps = {
   title: null,
   createdAt: null,
   createdBy: null,
+  mail: null,
 };
 
 export default NoteRow;
